8139ad4d65c99f158cb78d4a812eee2516145d44
San4si <san_juq@hotmail.com>
2021-05-04 19:16:20 -0300

    First commit

---
+++ src/negocio/Atividade.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package negocio;
+
+/**
+ *
+ * @author Sandro
+ */
+public class Atividade {
+   private int codAtv;
+   private String descricao;
+   
+   public Atividade (){
+   
+   }
+
+    public int getCodAtv() {
+        return codAtv;
+    }
+
+    public void setCodAtv(int codAtv) {
+        this.codAtv = codAtv;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+   
+   
+}
